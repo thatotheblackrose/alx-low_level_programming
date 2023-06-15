fa84bcf2da4ab01ed4368bd317e509e12a40c097
thatotheblackrose <thatothelancer@gmail.com>
2023-06-15 03:08:21 -0700

    4)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
+/*c program to print output*/
 #include <stdio.h>
-/**
- * main - function
- *
- * return: ALways 0 (Success)
- */
-int main(void)
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+int main(void) {
+    puts("Programming is like building a multilingual puzzle\n");
+    return 0;
+}
+
