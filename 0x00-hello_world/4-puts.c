93e093eeb93642a012a7775d30928de1c381ba39
thatotheblackrose <thatothelancer@gmail.com>
2023-06-15 03:57:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - function
+ * main - Enry point
  *
- * return: always 0 success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
