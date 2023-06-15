24f099942f795f8f8b721c134f814955bdb4c8a6
thatotheblackrose <thatothelancer@gmail.com>
2023-06-15 03:50:54 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * return: always 0 success
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
