7fe8858b4b271421dfd95cc8a5dc3b372502a2de
thatotheblackrose <thatothelancer@gmail.com>
2023-06-18 11:01:50 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,33 +8,33 @@
 
 int main(void)
 {
-    int i = '0';
-    int j = '0';
+int i = '0';
+int j = '0';
 
-    while (i <= '9')
-    {
-        while (j <= '9')
-        {
-            if (!(i > j) || i == j)
-            {
-                putchar(i);
-                putchar(j);
+while (i <= '9')
+{
+while (j <= '9')
+{
+if (!(i > j) || i == j)
+{
+putchar(i);
+putchar(j);
 
-                if (i == '8' && j == '9')
-                {
-                    putchar('\n');
-                }
-                else
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-            j++;
-        }
-        j = '0';
-        i++;
-    }
+if (i == '8' && j == '9')
+{
+putchar('\n'); 
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+j++;
+}
+j = '0';
+i++;
+}
 
-    return 0;
+return (0);
 }
