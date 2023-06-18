36849171fde3d3eeae435443b8955c7ca53223bf
thatotheblackrose <thatothelancer@gmail.com>
2023-06-18 09:55:51 -0700

    7p

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,17 +5,16 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char letter;
+    char letter;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-        {
-	putchar(letter);
-        }
-        
-        putchar('\n');
+    for (letter = 'z'; letter >= 'a'; letter--)
+    {
+        putchar(letter);
+    }
 
-	return (0);
+    putchar('\n');
+
+    return (0);
 }
