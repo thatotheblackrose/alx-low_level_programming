e3a734139eed777f747a9abad68c98bd33162477
thatotheblackrose <thatothelancer@gmail.com>
2023-06-21 09:01:16 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,37 +6,37 @@
  * @n: The number to start printing from
  * Return: void
  */
+
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            if (n == 98)
-            {
-                printf("%d\n", n);
-                break;
-            }
-            else
-            {
-                printf("%d, ", n);
-            }
-        }
-    }
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            if (n == 98)
-            {
-                printf("%d\n", n);
-                break;
-            }
-            else
-            {
-                printf("%d, ", n);
-            }
-        }
-    }
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%d\n", n);
+break;
 }
-
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d\n", n);
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+}
