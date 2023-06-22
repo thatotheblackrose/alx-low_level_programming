1c4d2fb78bf70b9901bb4e9ea1140debae1c8ea5
thatotheblackrose <thatothelancer@gmail.com>
2023-06-22 10:27:17 -0700

    102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,12 +9,13 @@
 void fibonacci(int n)
 {
     int fib_list[50];
+    int i;
+
     fib_list[0] = 1;
     fib_list[1] = 2;
 
     printf("%d, %d", fib_list[0], fib_list[1]);
 
-    int i;
     for (i = 2; i < n; i++)
     {
         fib_list[i] = fib_list[i - 1] + fib_list[i - 2];
