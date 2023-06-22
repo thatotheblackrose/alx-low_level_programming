83161df69d0a349dee27b0da9a0e76e566e05b02
thatotheblackrose <thatothelancer@gmail.com>
2023-06-22 10:48:48 -0700

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int fibonacci_sum(int limit) {
+    int a = 1;
+    int b = 2;
+    int sum_even = 0;
+
+    while (a <= limit) {
+        // Check if the current term is even
+        if (a % 2 == 0) {
+            sum_even += a;
+        }
+
+        // Generate the next Fibonacci term
+        int temp = a;
+        a = b;
+        b = temp + b;
+    }
+
+    return sum_even;
+}
+
+int main() {
+    int limit = 4000000;
+    int result = fibonacci_sum(limit);
+
+    printf("%d\n", result);
+
+    return 0;
+}
+
