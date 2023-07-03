3ed066882c99a8b4a9354a892b6f27b13e029b9b
thatotheblackrose <thatothelancer@gmail.com>
2023-07-03 06:11:13 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i, n = 0, sum1 = 0, sum2 = 0;
+int i, sum1 = 0, sum2 = 0;
 
-for (i = 0; i <= (size * size) - size; n = n + size - 1)
+for (i = 0; i < size * size; i = i + size + 1)
 sum1 = sum1 + a[i];
 
-for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-sum2 = sum2 + a[n];
+for (i = size - 1; i < size * size - 1; i = i + size - 1)
+sum2 = sum2 + a[i];
+
 printf("%d, %d\n", sum1, sum2);
 }
