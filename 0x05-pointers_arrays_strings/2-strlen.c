beaf51e2c8b475fa7403e4688465d20e6fc2c0fc
thatotheblackrose <thatothelancer@gmail.com>
2023-07-10 02:57:05 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of  string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
